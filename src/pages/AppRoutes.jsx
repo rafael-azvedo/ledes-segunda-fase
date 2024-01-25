a850a93aa33214ec52097a48e75722c4c60eaa9a
mariaEdCaldas <maria_caldas@ufms.br>
2024-01-24 22:56:23 -0400

    feat: Adding screen routes

---
+++ src/pages/AppRoutes.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import App from '../App';
+import Login from "./login/Login";
+import Cadastro from "./cadastro/Cadastro";
+import Cracha from "./cracha/Cracha";
+import Administrador from "./administrador/Administrador";
+import AlterarCracha from "./alterarCracha/AlterarCracha";
+import AlterarSenha from "./alterarSenha/AlterarSenha";
+
+
+const AppRoutes = () => {    
+    return (
+        <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/cadastro" element={<Cadastro/>}/>
+            <Route path="/cracha" element={<Cracha/>}/>
+            <Route path="/administrador" element={<Administrador/>}/>
+            <Route path="/alterarCracha" element={<AlterarCracha/>}/>
+            <Route path="/alterarSenha" element={<AlterarSenha/>}/>
+        </Routes>
+    );
+};
+export default AppRoutes;
