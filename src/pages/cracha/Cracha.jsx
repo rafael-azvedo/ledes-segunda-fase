af90c5c3b8b58e9181475c5d17b721331c429daa
mariaEdCaldas <maria_caldas@ufms.br>
2024-01-25 18:13:16 -0400

    feat: Adding badge page already with database search

--- src/pages/cracha/Cracha.jsx
+++ src/pages/cracha/Cracha.jsx
@@ -1,11 +1,29 @@
+import React, { useState, useEffect } from 'react';
 import './Cracha.css';
-import { useNavigate } from "react-router-dom";
-import React from 'react';
+import ufmsLogo from './ufmsLogo.png';
+
 
 const Cracha = () => {
+    const [dadosUsuario, setDadosUsuario] = useState({});
+
+    useEffect(() => {
+            fetch('localhost:8000/api/v1/badge/')
+            .then(response => response.json())
+            .then(data => setDadosUsuario(data))
+            .catch(error => console.error('Erro ao buscar dados do usuário:', error));
+    }, []);
+
     return (
-        <div>
-            <h2>Página de Crachá</h2>
+        <div className="body-cracha">
+            <div className="cracha-container">
+                {/* caminho da foto???????? */} 
+                <img src={`http://localhost:8000/media/ ${dadosUsuario.id}.jpg`} alt="Foto do Usuário" className="foto-usuario" />
+                <div className="info-usuario">
+                    <h2>{dadosUsuario.first_name} {dadosUsuario.last_name}</h2>
+                    <p>{dadosUsuario.role}</p>
+                </div>
+                <img src={ufmsLogo} className="logo" />
+            </div>
         </div>
     );
 };
