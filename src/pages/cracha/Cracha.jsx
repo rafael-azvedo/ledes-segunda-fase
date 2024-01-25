4dfb4730e450580de754222ac5e557d8712d2ec9
mariaEdCaldas <maria_caldas@ufms.br>
2024-01-24 22:56:49 -0400

    feat: Adding badge screen

---
+++ src/pages/cracha/Cracha.jsx
@@ -0,0 +1,13 @@
+import './Cracha.css';
+import { useNavigate } from "react-router-dom";
+import React from 'react';
+
+const Cracha = () => {
+    return (
+        <div>
+            <h2>Página de Crachá</h2>
+        </div>
+    );
+};
+
+export default Cracha;
